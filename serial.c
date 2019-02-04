cc2c8cb078cc174f1dc2c5d2c6cebe89e1d4385f
Lars Kellogg-Stedman <lars@redhat.com>
2019-02-04 14:56:49 -0500

    code formatting

--- serial.c
+++ serial.c
@@ -37,16 +37,16 @@
 #if ((F_CPU/SERIAL_BPS/8) < 256)
 #pragma message "prescaler: CLK/8"
 #define PRESCALER_FLAG 0b010
-#define PRESCALER_VAL 8
+#define PRESCALER_VAL  8
 #else
 #if ((F_CPU/SERIAL_BPS/64) < 256)
 #pragma message "prescaler: CLK/64"
 #define PRESCALER_FLAG 0b011
-#define PRESCALER_VAL 64
+#define PRESCALER_VAL  64
 #else
 #pragma message "prescale: CLK/256"
 #define PRESCALER_FLAG 0b100
-#define PRESCALER_VAL 256
+#define PRESCALER_VAL  256
 #endif
 #endif
 #endif
@@ -91,8 +91,8 @@ volatile struct SERIAL_PORT {
 #define DEBUGPIN PORTB1
 
 const struct avr_mmcu_vcd_trace_t _mytrace[]  _MMCU_ = {
-    { AVR_MCU_VCD_SYMBOL("TX"), .mask = (1<<SERIAL_TXPIN), .what = (void*)&SERIAL_TXPORT,  },
-    { AVR_MCU_VCD_SYMBOL("ISR"), .mask = (1<<DEBUGPIN), .what = (void*)&DEBUGPORT,  },
+    { AVR_MCU_VCD_SYMBOL("TX"),  .mask = (1<<SERIAL_TXPIN), .what = (void*)&SERIAL_TXPORT, },
+    { AVR_MCU_VCD_SYMBOL("ISR"), .mask = (1<<DEBUGPIN),     .what = (void*)&DEBUGPORT,     },
 };
 #endif
 
@@ -116,14 +116,14 @@ millis_t _millis;
 
 void serial_init() {
 #ifdef DEBUG
-    DEBUGDDR |= 1<<DEBUGPIN;
+    DEBUGDDR      |= 1<<DEBUGPIN;
 #endif
 
-    SERIAL_TXDDR |= 1<<SERIAL_TXPIN;   // Set SERIAL_TXPIN as an output
-    SERIAL_TXPORT |= 1<<SERIAL_TXPIN;  // Set SERIAL_TXPIN high (serial idle)
-    TCCR0A = 1<<WGM01;                 // Select CTC mode
-    TCCR0B = PRESCALER_FLAG;           // Set clock prescaler
-    OCR0A = TICKS_PER_BIT;             // Set CTC target value
+    SERIAL_TXDDR  |= 1<<SERIAL_TXPIN;   // Set SERIAL_TXPIN as an output
+    SERIAL_TXPORT |= 1<<SERIAL_TXPIN;   // Set SERIAL_TXPIN high (serial idle)
+    TCCR0A         = 1<<WGM01;          // Select CTC mode
+    TCCR0B         = PRESCALER_FLAG;    // Set clock prescaler
+    OCR0A          = TICKS_PER_BIT;     // Set CTC target value
     sei();
 }
 
@@ -139,9 +139,9 @@ void serial_end() {
 void serial_putchar(char c) {
     while (port.busy);      // Wait for previous byte to complete
 
-    port.data = c;
+    port.data  = c;
     port.index = 0;
-    port.busy = 1;
+    port.busy  = 1;
 }
 
 void serial_print(char *s) {
